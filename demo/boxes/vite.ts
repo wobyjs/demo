5c745178852b3243ada1b09c922589c56e6f61d4
wongchichong <wongchichong@gmail.com>
2023-02-14 13:15:15 +0800

    first commit

---
+++ demo/boxes/vite.ts
@@ -0,0 +1,24 @@
+
+/* IMPORT */
+
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import voby from 'voby-vite'
+
+/* MAIN */
+
+const config = defineConfig({
+    plugins: [
+        voby()
+    ],
+    resolve: {
+        alias: {
+            '~': path.resolve(__dirname, '../../../voby/src'),
+            voby: process.argv.includes('dev') ? path.resolve(__dirname, '../../../voby/src') : 'voby'
+        }
+    }
+})
+
+/* EXPORT */
+
+export default config
