a4d2cd61a8dc5f7b0de34cf5a4efd3f52032dd7b
wongchichong <wongchichong@gmail.com>
2023-03-13 15:35:54 +0800

    update with via

--- demo/counter/index.tsx
+++ demo/counter/index.tsx
@@ -7,19 +7,19 @@ import type { JSX } from 'voby'
 
 const Counter = (): JSX.Element => {
 
-  const value = $(0)
+    const value = $(0)
 
-  const increment = () => value(prev => prev + 1)
-  const decrement = () => value(prev => prev - 1)
+    const increment = () => value(prev => prev + 1)
+    const decrement = () => value(prev => prev - 1)
 
-  return (
-    <>
-      <h1>Counter</h1>
-      <p>{value}</p>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-    </>
-  )
+    return (
+        <>
+            <h1>Counter</h1>
+            <p>{value}</p>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+        </>
+    )
 }
 
 render(<Counter />, document.getElementById('app'))
